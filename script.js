a16b2000b0249982d1adfec711c7c4a864636c83
goldendog92 <60197505+goldendog92@users.noreply.github.com>
2020-05-09 19:54:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+//click the images to scroll
+
+
+
+
+
+
+
+
+/*
+var i = 0; // start point
+var images = [];
+var time = 7000;
+
+
+images[0] = 'images/image0.jpeg';
+images[1] = 'images/image1 .jpeg';
+images[2] = 'images/image2 .jpeg';
+images[3] = 'images/image3 .jpeg';
+images[4] = 'images/image4 .jpeg';
+
+function changeImg(){
+  document.images.src = images[i];
+  if (i < images.length - 1){
+    i++;
+  } else {
+    i = 0
+  }
+  setTimeout ("changeImg()", time);
+}
+
+window.onload = changeImg;
+*/
+
+
+document.querySelector("i")
+.addEventListener('click', () => {
+  window.location.href = "https://www.instagram.com/thetravellinggolden/";
+})
+
+
+
+//var instagram = document.getElementById('fab fa-instagram fa-3x');
+
+
+//instagram.addEventListener("click", function(){window.location.href="https://www.instagram.com/thetravellinggolden/";})
+
+
+//function instagram() {
+  //document.getElementsByClassName('i').addEventListener('onclick')
+  //.window.location.href="https://www.instagram.com/thetravellinggolden/";
+//};
+
+
+
+//(i).addEventListener('click',
+//function () {
+  //window.location.href = "https://www.instagram.com/thetravellinggolden/";
+//});
+
+
+//var instagram = document.getElementsByTagName('i')
+
+//instagram.addEventListener("click"), function() {
+//  location = "https://www.instagram.com/thetravellinggolden/";
+//
+
+
+
+
+
+/*const nav = document.querySelector("mainMenu");
+
+function stickyNavigation() {
+  console.log("navTop =" + navTop);
+  console.log("scrollY=" + window.scrollY);
+}
+
+window.addEventListener("scroll", stickyNavigation);
+
+function stickyNavigation() {
+  if (window.scrollY >= navTop) {
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.classList.remove("fixed-nav");
+  }
+}
+
+window.addEventListener("scroll", stickyNavigation);
+
+var button1 = document.getElementById("button1");
+button1.addEventListener("click", function () {
+  window.location = "about.html";
+});
+*/
